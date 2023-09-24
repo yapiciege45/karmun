ae0bf25cdff20629efa73b52b9e922a1ed0f76b0
Ege Yapıcı <yapiciege45@gmail.com>
2023-09-25 00:31:00 +0300

    home top page

--- src/components/home/HomeTopComponent.js
+++ src/components/home/HomeTopComponent.js
@@ -1,4 +1,5 @@
 "use client"
+import { IconChevronDown } from '@tabler/icons-react'
 import Image from 'next/image'
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
@@ -6,7 +7,7 @@ import { Typewriter } from 'react-simple-typewriter'
 export const HomeTopComponent = () => {
   return (
     <div className="w-full h-screen bg-[url('/home-bg.jpeg')] bg-center relative">
-      <div className='absolute top-0 left-0 h-screen bg-black/50 w-full z-40 flex justify-center items-center'>
+      <div className='absolute top-0 left-0 h-screen bg-black/50 w-full z-10 flex justify-center items-center'>
         <div className='flex flex-col items-center w-full'>
             <Image 
               src="/karmun_beyaz.png"
@@ -22,6 +23,18 @@ export const HomeTopComponent = () => {
               />
             </p>
         </div>
+      </div>
+      <div className='absolute z-20 bottom-2 left-1/2 flex flex-col' style={{transform: 'translate(-50%,-50%)'}}>
+            <p className='uppercase text-white text-lg mt-2 font-light'>
+              <Typewriter 
+                words={['Do you want to see more?',"Let's scroll!","You need to be EXCITED!"]}
+                typeSpeed={70}
+                loop
+              />
+            </p>
+            <div className='w-full flex justify-center animate-bounce'>
+              <IconChevronDown color="white" />
+            </div>
       </div>
     </div>
   )
