7bf92af00252c8e6d80bdc95ccea77c706f44cc5
Ege Yapıcı <yapiciege45@gmail.com>
2023-10-06 00:17:36 +0300

    unsc fix

--- src/components/home/HomeTopComponent.js
+++ src/components/home/HomeTopComponent.js
@@ -1,6 +1,7 @@
 "use client"
 import { IconChevronDown } from '@tabler/icons-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 
