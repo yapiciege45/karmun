b27a8de5fa8371cf27580d22881b612928056e62
Ege Yapıcı <yapiciege45@gmail.com>
2023-10-08 15:49:15 +0300

    home bg change and kerem arslan added

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
-      <body className="min-h-[calc(100dvh)] bg-[url('/home-bg.jpeg')] bg-center bg-cover relative w-full overflow-x-hidden">{children}</body>
+      <body className="bg-no-repeat bg-[url('/home-bg.jpeg')] bg-center bg-cover relative w-full overflow-x-hidden">
+        <div className='h-[calc(100dvh)] overflow-y-scroll'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
